12b072fd76fa5c615428070597aaafe896316c84
C.J. May <maycab@uni.edu>
2018-09-18 13:36:55 -0500

    import stack

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,10 @@
 
+from sys import path
+import os
+path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.linkedStack import LinkedStack
 
 class Parser:
     def __init__(self):
-        stack = linkedStack()
+        stack = LinkedStack()
         stack.push("$")
