673d31b258df0c662b5c9bfb3e77f77b8f6b1a14
Nick Sanford <sanfordn@uni.edu>
2018-09-26 18:48:32 -0500

    Started working on parser. Going to open up the whole project folder

--- src/parser.py
+++ src/parser.py
@@ -4,6 +4,10 @@ import os
 path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.linkedStack import LinkedStack
 
+class NonTerminal(Enum):
+    Program     = 0
+    
+
 class Parser:
     def __init__(self):
         stack = LinkedStack()
