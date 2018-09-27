729dff0ccc498571f539b2fe5613bb9c1314ad0e
Nick Sanford <sanfordn@uni.edu>
2018-09-26 19:09:02 -0500

    Listed out nonterminals

--- src/parser.py
+++ src/parser.py
@@ -6,7 +6,29 @@ from src.linkedStack import LinkedStack
 
 class NonTerminal(Enum):
     Program     = 0
-    
+    Definitions = 1
+    Def         = 2
+    Formals     = 3
+    Nonempty_Formals = 4
+    Formal      = 5
+    Body        = 6
+    Statement_List = 7
+    Type        = 8
+    Expr        = 9
+    Simple_Expr = 10
+    Term        = 11
+    Factor      = 12
+    Actuals     = 13
+    Nonempty_Actuals = 14
+    Literal     = 15
+
+
+
+
+
+
+
+
 
 class Parser:
     def __init__(self):
