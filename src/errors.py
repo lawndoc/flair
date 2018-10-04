b5f3fe495fe952fc90095b5940c390489c9b1f66
C.J. May <maycab@uni.edu>
2018-10-04 14:03:37 -0500

    reverted
    Co-Authored-By: null <sanfordn@users.noreply.github.com>

--- src/errors.py
+++ src/errors.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 # errors thrown by the scanner
 class LexicalError(ValueError):
-    def __str__(self):
-        return "Lexical Error: " + ValueError
+    pass
 
 # errors thrown by the parser
 class ParseError(ValueError):
-    def __str__(self):
-        return "Parse Error: " + ValueError
+    pass
 
 # errors thrown by the type checker
 class SemanticError(ValueError):
-    def __str__(self):
-        return "Semantic Error: " + ValueError
+    pass
