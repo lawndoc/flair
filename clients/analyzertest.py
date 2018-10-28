0ef9979031c53202cac67b5435361a784e4e7134
C.J. May <maycab@uni.edu>
2018-10-28 00:27:36 -0500

    created clients for analyzer

---
+++ clients/analyzertest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sys import argv, path
+import os
+path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.parser import Parser
+from src.scanner import Scanner
+from src.analyzer import Analyzer
+
+# pass in arg for path to flair program
+if len(argv) > 1:
+    if "analyzertest.py" in argv[0]:
+        FILE_PATH = argv[1]
+    elif "analyzertest.py" in argv[1]:
+        FILE_PATH = argv[2]
+    else:
+        error_msg = ("Unexpected call from the command line: {}")
+        raise SyntaxError(error_msg.format(" ".join(argv)))
+else:
+    error_msg = ("Please pass an arg for the path to a flair program.")
+    raise SyntaxError(error_msg)
+
+# store program into string variable
+with open(FILE_PATH, "r") as flr:
+    flairProgram = flr.read()
+
+scanner = Scanner(flairProgram)
+parser = Parser(scanner)
+analyzer = Analyzer(parser.parse())
