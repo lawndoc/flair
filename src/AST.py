592a4ebf06417428bd8b579d92423a537bfb9359
C.J. May <maycab@uni.edu>
2018-11-29 21:48:53 -0600

    test fix

--- src/AST.py
+++ src/AST.py
@@ -1050,13 +1050,13 @@ class FunctionCall(ASTnode):
         code += "* reset offset to -7\n"
         # optionally generate code if function gives arguments
         if len(symbolTable[self.getName()].getFormals()) > 0:
-            code += lineRM(symbolTable,"LDC",1,1,0,"load 1 into r1 for decrementing r6")
             for i in range(0,len(symbolTable[self.getName()].getFormals())):
                 # TODO: generate code for actual : "LDC" <--> "LD", getValue() <--> getValueOffset()
                 code = self.actuals[i].genCode(symbolTable, code, fName)
                 code += lineRM(symbolTable,"LD",2,self.actuals[i].getValueOffset(),5,"load arg{} into r2".format(str(i+1)))
                 # code += lineRM(symbolTable,"LDC",2,self.actuals[i].getValue(),0,"load arg{} into r2".format(str(i+1)))
                 code += lineRM(symbolTable,"ST",2,-(i+8),5,"load arg{} into AR".format(str(i+1)))
+                code += lineRM(symbolTable,"LDC",1,1,0,"load 1 into r1 for decrementing r6")
                 code += lineRO(symbolTable,"SUB",6,6,1,"decrement end of stack pointer")
                 symbolTable.decrementOffset()# ; code += "* offset: " + str(symbolTable.getOffset()) + "\n"
         # add return address to function's AR
